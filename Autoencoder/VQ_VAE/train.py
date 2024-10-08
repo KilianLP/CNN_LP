341e160f4a8ffc4093129cd95f5276eaa39144bc
KilianLP <157063461+KilianLP@users.noreply.github.com>
2024-10-08 19:33:08 +0200

    Create train.py

---
+++ Autoencoder/VQ_VAE/train.py
@@ -0,0 +1,39 @@
+import torch.optim as optim
+import torch.nn as nn
+
+device = T.device("cuda" if T.cuda.is_available() else "cpu")
+
+autoencoder = Autoencoder(512)
+autoencoder.to(device)
+
+loss_fn = nn.MSELoss()
+optimizer = optim.Adam(autoencoder.parameters(), lr = 1e-3)
+
+for _ in range(40):
+  total_loss = 0
+  autoencoder.train()
+  
+  for batch,_ in dataloader:
+    optimizer.zero_grad()
+    batch = batch.to(device)
+    
+    output,loss = autoencoder(batch)
+    loss_value = loss_fn(output,batch) + loss
+    loss_value.backward()
+    
+    optimizer.step()
+    
+    total_loss += loss_value.item()
+    
+  print("train_loss =",total_loss/len(dataloader))
+
+  autoencoder.eval()
+  total_loss = 0
+  
+  with T.no_grad():
+    for batch,_ in dataloader_test:
+      batch = batch.to(device)
+      output,_ = autoencoder(batch)
+      loss_value = loss_fn(output,batch)
+      total_loss += loss_value.item()
+  print("test_loss =",total_loss/len(dataloader_test))
